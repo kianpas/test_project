3da2a2e592ef4851b68e3e897080254a23951228
kianpas <kianpas@gmail.com>
2022-01-23 16:28:20 +0900

    22.01.23 페이징 처리 추가

--- pybo/views.py
+++ pybo/views.py
@@ -6,7 +6,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
 from .forms import AnswerForm, QuestionForm
+from django.core.paginator import Paginator
 
+MAX_LIST_CNT = 10
+MAX_PAGE_CNT = 5
 
 # Create your views here.
 
@@ -15,8 +18,31 @@ def index(request):
     """
     pybo 목록 출력
     """
+    # page 번호
+    page = request.GET.get("page", "1")
+    # 질문 리스트
     question_list = Question.objects.order_by("-create_date")
-    context = {"question_list": question_list}
+    # 페이징 처리, 페이징 처리할 리스트와 페이지당 수
+    paginator = Paginator(question_list, MAX_LIST_CNT)
+
+    last_page_num = 0
+    for last_page in paginator.page_range:
+        last_page_num = last_page_num + 1
+
+    current_block = ((int(page) - 1) / MAX_PAGE_CNT) + 1
+    current_block = int(current_block)
+
+    page_start_number = ((current_block - 1) * MAX_PAGE_CNT) + 1
+
+    page_end_number = page_start_number + MAX_PAGE_CNT - 1
+
+    page_obj = paginator.get_page(page)
+    context = {
+        "question_list": page_obj,
+        "list_page_num": last_page_num,
+        "page_start_number": page_start_number,
+        "page_end_number": page_end_number,
+    }
     return render(request, "pybo/question_list.html", context)
 
 
@@ -45,7 +71,8 @@ def answer_create(request, question_id):
     else:
         form = AnswerForm()
     context = {"question": question, "form": form}
-    return render(request, 'pybo/question_detail.html', context)     
+    return render(request, "pybo/question_detail.html", context)
+
 
 def question_create(request):
     """
