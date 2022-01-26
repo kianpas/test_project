a902ade5d9a1c56389ba4c16a398cc8206f1d138
kianpas <kianpas@gmail.com>
2022-01-26 16:16:42 +0900

    22.01.26 추천, 검색 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-
+from django.db.models import Q, Count
 from ..models import Question
 
 MAX_LIST_CNT = 10
@@ -12,8 +12,29 @@ def index(request):
     """
     # page 번호
     page = request.GET.get("page", "1")
+    kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬기준
+
+       # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
+
+
     # 질문 리스트
-    question_list = Question.objects.order_by("-create_date")
+    # question_list = Question.objects.order_by("-create_date")
+
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
+
     # 페이징 처리, 페이징 처리할 리스트와 페이지당 수
     paginator = Paginator(question_list, MAX_LIST_CNT)
 
@@ -34,6 +55,8 @@ def index(request):
         "list_page_num": last_page_num,
         "page_start_number": page_start_number,
         "page_end_number": page_end_number,
+        "kw": kw,
+        'so': so
     }
     return render(request, "pybo/question_list.html", context)
 
